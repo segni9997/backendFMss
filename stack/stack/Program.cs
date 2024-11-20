78e8fc3a1d5a6f58da61aaa648b54751e85a15a9
segni <102437901+segni9997@users.noreply.github.com>
2024-11-20 10:02:41 +0300

    Delete stack directory

--- stack/stack/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Stack is for value type and a Heap is for reference types
-int testValues = 4;
-byte lowLevelValue = 3;
-byte highLevelValue = 2;
-string stringValue = "sting";
-
-Console.WriteLine(stringValue);
